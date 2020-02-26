00177d4beb7a2fecb1dfd90fc4868b1e95f33dc1
Andrew Reid <areid.sj@gmail.com>
2020-02-26 14:57:41 -0700

    Write and pass key exists test

--- lib/key.rb
+++ lib/key.rb
@@ -1,4 +1,7 @@
 class Key
 
+  def initialize(random_number)
+    @random_number = random_number
+  end
 
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -4,4 +4,12 @@ require './lib/key.rb'
 
 class KeyTest < Minitest::Test
 
+  def setup
+    @key = Key.new("02715")
+  end
+
+  def test_it_exists
+    assert_instance_of Key, @key
+  end
+
 end
