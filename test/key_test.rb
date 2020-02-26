3f8b8275f0e28c55895bbdaf5f30e6662251204f
Andrew Reid <areid.sj@gmail.com>
2020-02-26 15:20:49 -0700

    Add additional test coverage for key class

--- test/key_test.rb
+++ test/key_test.rb
@@ -5,7 +5,8 @@ require './lib/key.rb'
 class KeyTest < Minitest::Test
 
   def setup
-    @key = Key.new("A", "02")
+    @key1 = Key.new("A", "02")
+    @key2 = Key.new("C", "79")
   end
 
   def test_it_creates_keys
@@ -19,12 +20,15 @@ class KeyTest < Minitest::Test
   end
 
   def test_it_exists
-    assert_instance_of Key, @key
+    assert_instance_of Key, @key1
+    assert_instance_of Key, @key2
   end
 
   def test_it_has_attributes
-    assert_equal "A", @key.key_letter
-    assert_equal "02", @key.key_number
+    assert_equal "A", @key1.key_letter
+    assert_equal "02", @key1.key_number
+    assert_equal "C", @key2.key_letter
+    assert_equal "79", @key2.key_number
   end
 
 end
