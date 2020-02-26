3e766ab96d262eb2cb7c990caa9516489f2b8591
Andrew Reid <areid.sj@gmail.com>
2020-02-26 14:45:17 -0700

    Write and pass test alphabet exists

--- test/alphabet_test.rb
+++ test/alphabet_test.rb
@@ -1,8 +1,15 @@
 require 'minitest/autorun'
-require 'minitest/mocha'
+require 'minitest/pride'
 require './lib/alphabet.rb'
 
 class AlphabetTest < Minitest::Test
 
+  def setup
+    @alpha = Alphabet.new
+  end
+
+  def test_it_exists
+    assert_instance_of Alphabet, @alpha
+  end
 
 end
