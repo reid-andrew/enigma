1477adaea2ea444c4d7da8b0f74dab34c0cd7cd5
Andrew Reid <areid.sj@gmail.com>
2020-02-26 15:19:07 -0700

    Refactor key methods to create key letter

--- lib/key.rb
+++ lib/key.rb
@@ -5,20 +5,23 @@ class Key
   def self.create_keys(random_number)
     @@all_keys = []
     counter = 0
+    letters = "ABCD"
     while counter < 4
-      @@all_keys << Key.new("#{random_number[counter]}#{random_number[counter + 1]}")
+      @@all_keys << Key.new(letters[counter], "#{random_number[counter]}#{random_number[counter + 1]}")
       counter += 1
     end
-  end  
+  end
 
   def self.all
     @@all_keys
   end
 
-  attr_reader :random_number
+  attr_reader :key_letter,
+              :key_number
 
-  def initialize(random_number)
-    @random_number = random_number
+  def initialize(key_letter, key_number)
+    @key_letter = key_letter
+    @key_number = key_number
   end
 
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -5,16 +5,17 @@ require './lib/key.rb'
 class KeyTest < Minitest::Test
 
   def setup
-    @key = Key.new("02")
+    @key = Key.new("A", "02")
   end
 
   def test_it_creates_keys
     Key.create_keys("02715")
 
     assert_equal 4, Key.all.size
-    assert_equal "02", Key.all[0].random_number
-    assert_equal "15", Key.all[-1].random_number
-    require "pry"; binding.pry
+    assert_equal "02", Key.all[0].key_number
+    assert_equal "A", Key.all[0].key_letter
+    assert_equal "15", Key.all[-1].key_number
+    assert_equal "D", Key.all[-1].key_letter
   end
 
   def test_it_exists
@@ -22,7 +23,8 @@ class KeyTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_equal "02", @key.random_number
+    assert_equal "A", @key.key_letter
+    assert_equal "02", @key.key_number
   end
 
 end
