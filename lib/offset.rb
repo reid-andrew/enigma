a148155ae98a3efc21ae8926698eb69ac314c9cf
Andrew Reid <areid.sj@gmail.com>
2020-02-26 17:19:17 -0700

    Write and pass offset exists tests

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,4 +1,11 @@
-class Offset
+class ShiftOffset
 
+  attr_reader :offset_letter,
+              :offset_number
+
+  def initialize(offset_letter, offset_number)
+    @offset_letter = offset_letter
+    @offset_number = offset_number
+  end
 
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -2,7 +2,16 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/offset.rb'
 
-class Offset < Minitest::Test
+class ShiftOffsetTest < Minitest::Test
 
+  def setup
+    @offset1 = ShiftOffset.new("A", 1)
+    @offset2 = ShiftOffset.new("C", 0)
+  end
+
+  def test_it_exists
+    assert_instance_of ShiftOffset, @offset1
+    assert_instance_of ShiftOffset, @offset2
+  end
 
 end
