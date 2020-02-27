0dbfa67c8d76abf7514a36fbfa2103b04040ae7a
Andrew Reid <areid.sj@gmail.com>
2020-02-26 17:40:54 -0700

    Write and pass test for create offsets and related helper methods

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,5 +1,34 @@
 class ShiftOffset
 
+  @@all_offsets = nil
+
+  def self.create_offsets(date_input)
+    @@all_offsets = []
+    counter = 1
+    letters = "ABCD"
+    number = truncate_input(square_input(date_input))
+    while counter < 5
+      @@all_offsets << create_single_offset(letters, number, counter)
+      counter += 1
+    end
+  end
+
+  def self.square_input(date_input)
+    date_input.to_i * date_input.to_i
+  end
+
+  def self.truncate_input(sqaured_input)
+    sqaured_input.to_s[-4..-1].to_i
+  end
+
+  def self.create_single_offset(letters, number, counter)
+    ShiftOffset.new(letters[counter - 1], number.to_s[counter - 5].to_i)
+  end
+
+  def self.all
+    @@all_offsets
+  end
+
   attr_reader :offset_letter,
               :offset_number
 
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -9,6 +9,36 @@ class ShiftOffsetTest < Minitest::Test
     @offset2 = ShiftOffset.new("C", 0)
   end
 
+  def test_it_creates_offsets
+    ShiftOffset.create_offsets("040895")
+
+    assert_equal 4, ShiftOffset.all.size
+    assert_equal 1, ShiftOffset.all[0].offset_number
+    assert_equal "A", ShiftOffset.all[0].offset_letter
+    assert_equal 5, ShiftOffset.all[-1].offset_number
+    assert_equal "D", ShiftOffset.all[-1].offset_letter
+  end
+
+  def test_it_squares_input
+    assert_equal 1672401025, ShiftOffset.square_input("040895")
+  end
+
+  def test_it_truncates_input
+    assert_equal 1025, ShiftOffset.truncate_input(1672401025)
+  end
+
+  def test_it_creates_single_offset
+    key1 = ShiftOffset.create_single_offset("ABCD", 1025, 1)
+
+    assert_equal 1, key1.offset_number
+    assert_equal "A", key1.offset_letter
+
+    key2 = ShiftOffset.create_single_offset("ABCD", 1025, 4)
+
+    assert_equal 5, key2.offset_number
+    assert_equal "D", key2.offset_letter
+  end
+
   def test_it_exists
     assert_instance_of ShiftOffset, @offset1
     assert_instance_of ShiftOffset, @offset2
