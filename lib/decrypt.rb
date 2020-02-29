f766914dd3ab67e1bb8b6d02269eca1c3e93fb3b
Andrew Reid <areid.sj@gmail.com>
2020-02-29 07:28:50 -0700

    Refactor to account for no date parameter input

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -3,7 +3,8 @@ require './lib/enigma.rb'
 enigma = Enigma.new
 source = File.open(ARGV[0], "r")
 destination = File.open(ARGV[1], "w")
-encrypted_message = enigma.decrypt(source.read, ARGV[2], ARGV[3])
+date = ARGV[3] ? ARGV[3] : Date.today
+encrypted_message = enigma.decrypt(source.read, ARGV[2], date)
 
 destination.write(encrypted_message[:encryption])
 puts "Created '#{File.basename(destination)}' with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
