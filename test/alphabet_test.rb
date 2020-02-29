d79ffb001e36c1a9a11fd0f9eaa53c89ce6c7757
Andrew Reid <areid.sj@gmail.com>
2020-02-29 05:59:56 -0700

    Remove irrelevant alphabet tests

--- test/alphabet_test.rb
+++ test/alphabet_test.rb
@@ -18,32 +18,4 @@ class AlphabetTest < Minitest::Test
     assert_equal "f", Alphabet.alpha_by_number[6]
   end
 
-  # def setup
-  #   @alpha = Alphabet.new
-  # end
-  #
-  # def test_it_exists
-  #   assert_instance_of Alphabet, @alpha
-  # end
-  #
-  # def test_it_has_attributes
-  #   expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
-  #   assert_equal expected, @alpha.alphabet
-  # end
-  #
-  # def test_it_creates_alphabet
-  #   expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
-  #   assert_equal expected, @alpha.create_alphabet
-  # end
-  #
-  # def test_it_creates_alphabet_by_letter
-  #   assert_equal 3, @alpha.alpha_by_letter["c"]
-  #   assert_equal 6, @alpha.alpha_by_letter["f"]
-  # end
-  #
-  # def test_it_creates_alphabet_by_number
-  #   assert_equal "c", @alpha.alpha_by_number[3]
-  #   assert_equal "f", @alpha.alpha_by_number[6]
-  # end
-
 end
