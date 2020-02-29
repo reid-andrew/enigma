d99245bd1392bf2019130a108b128eef256f1ef9
Andrew Reid <areid.sj@gmail.com>
2020-02-29 06:17:09 -0700

    Add logic to encrypt runner

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,8 +3,9 @@ require './lib/enigma.rb'
 enigma = Enigma.new
 source = File.open(ARGV[0], "r")
 destination = File.open(ARGV[1], "w")
-# require "pry"; binding.pry
 encrypted_message = enigma.encrypt(source.read)
-destination.write(encrypted_message)
+
+destination.write(encrypted_message[:encryption])
+puts "Created '#{File.basename(destination)}' with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
 source.close
 destination.close
